48e50451131ba6e1d5c558ad7a59dfc595dc37fb
webward09 <colmward@gmail.com>
2015-03-11 22:27:29 +0000

    Some code tweaks. Removed temporary code files

--- contact_client.py
+++ contact_client.py
@@ -4,20 +4,20 @@ _TIMEOUT_SECONDS = 10
 
 # Iterates though all people in the AddressBook and prints info about them.
 def ListPeople(address_book):
-  for person in address_book.person:
-    print "\nPerson ID:", person.id
-    print "  Name:", person.name
-    if person.HasField('email'):
-      print "  E-mail address:", person.email
+    for person in address_book.person:
+        print "\nPerson ID:", person.id
+        print "  Name:", person.name
+        if person.HasField('email'):
+            print "  E-mail address:", person.email
 
-    for phone_number in person.phone:
-      if phone_number.type == addressbook_pb2.Person.MOBILE:
-        print "  Mobile phone #:",
-      elif phone_number.type == addressbook_pb2.Person.HOME:
-        print "  Home phone #:",
-      elif phone_number.type == addressbook_pb2.Person.WORK:
-        print "  Work phone #:",
-      print phone_number.number
+        for phone_number in person.phone:
+            if phone_number.type == addressbook_pb2.Person.MOBILE:
+                print "  Mobile phone #:",
+            elif phone_number.type == addressbook_pb2.Person.HOME:
+                print "  Home phone #:",
+            elif phone_number.type == addressbook_pb2.Person.WORK:
+                print "  Work phone #:",
+            print phone_number.number
 
 # This function fills in a Person message based on user input.
 def PromptForAddress(person):
